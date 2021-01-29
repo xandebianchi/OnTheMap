c0cb66378d511c77b54622ccddbed215e54a0f8d
Alexandre Bianchi <alexandre.bianchi@ab-inbev.com>
2021-01-28 21:06:42 -0300

    Changes due to Code Review

--- OnTheMap/Controller/LoginViewController.swift
+++ OnTheMap/Controller/LoginViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
     // MARK: - UIControls
     
@@ -15,7 +15,26 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-        
+       
+    // MARK: - Properties
+    
+    var keyboardIsVisible = false
+    
+    override func viewDidLoad() {
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
     // MARK: - Actions
 
     @IBAction func signUpTapped(_ sender: Any) {
@@ -25,6 +44,42 @@ class LoginViewController: UIViewController {
     @IBAction func loginTapped(_ sender: Any) {
         setLoggingIn(true)
         UdacityClient.login(username: self.emailTextField.text ?? "", password: self.passwordTextField.text ?? "", completion: handleLoginResponse(success:error:))
+    }
+    
+    // MARK: - Functions related to keyboard presentation
+    
+    // Function called when keyboard must be shown and the screen must be moved up
+    @objc func keyboardWillShow(_ notification:Notification) {
+        if !keyboardIsVisible && (emailTextField.isEditing || passwordTextField.isEditing) {
+            view.frame.origin.y -= loginButton.frame.height
+            keyboardIsVisible = true
+        }
+    }
+    
+    // Function called when screen must be moved down
+    @objc func keyboardWillHide(_ notification:Notification) {
+        if keyboardIsVisible {
+            view.frame.origin.y += loginButton.frame.height
+            keyboardIsVisible = false
+        }
+    }
+    
+    // MARK: - Subscribe and unsubscribe from keyboard notifications
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    // MARK: - Text field delegate functions
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return true
     }
     
     // MARK: - Main methods
